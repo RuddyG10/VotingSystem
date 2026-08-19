ffa99c993bb82e52319cef83fc0f2aab62b452a4
RuddyG10 <rggt0001@ce.pucmm.edu.do>
2026-08-19 11:08:16 -0400

    creada la primera fase de la clase Vote

--- Domain/Entities/Vote.cs
+++ Domain/Entities/Vote.cs
@@ -4,7 +4,12 @@ using System.Text;
 
 namespace Domain.Entities
 {
-    internal class Vote
+    public class Vote
     {
+        public Guid Id { get; set; }
+        public Guid ElectionId { get; set; }
+        public Guid CandidateId { get; set; }
+        public Guid UserId { get; set; }
+        public DateTime CreatedAt { get; set; }
     }
 }
