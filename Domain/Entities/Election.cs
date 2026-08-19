9ddf8f6114c880514388c52aa80e419513be9630
RuddyG10 <rggt0001@ce.pucmm.edu.do>
2026-08-19 11:06:18 -0400

    creada la primera fase de la clase election

--- Domain/Entities/Election.cs
+++ Domain/Entities/Election.cs
@@ -4,7 +4,15 @@ using System.Text;
 
 namespace Domain.Entities
 {
-    internal class Election
+    public class Election
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool IsActive { get; set; }
+
     }
 }
