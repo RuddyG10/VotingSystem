85d8ce1326134207a6a38dbeaaff5176a50358c9
RuddyG10 <rggt0001@ce.pucmm.edu.do>
2026-08-25 14:22:16 -0400

    feat: add candidate service interface

---
+++ Application/Interfaces/ICandidateService.cs
@@ -0,0 +1,16 @@
+﻿using Application.DTOs.Candidates;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Interfaces
+{
+    public interface ICandidateService
+    {
+        Task<IReadOnlyList<CandidateResponse>> GetByElectionIdAsync(Guid electionId);
+        Task<CandidateResponse?> GetByIdAsync(Guid id);
+        Task<CandidateResponse?> CreateAsync(Guid id,CreateCandidateRequest request);
+        Task<CandidateResponse?> UpdateAsync(Guid id, UpdateCandidateRequest request);
+        Task<bool> DeleteAsync(Guid id);
+    }
+}
--- Application/obj/Debug/net10.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net10.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb7b7b359c34aa5359d9225f677f87b7c5e73d3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9094b7061b9ab6356dfb2274787528bedd28f3f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
