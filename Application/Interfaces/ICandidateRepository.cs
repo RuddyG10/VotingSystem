1b0307ba93c1246d3bc032c11c8d572752e2727f
RuddyG10 <rggt0001@ce.pucmm.edu.do>
2026-08-25 14:13:26 -0400

    feat: add candidate repository interface

---
+++ Application/Interfaces/ICandidateRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Interfaces
+{
+    public interface ICandidateRepository
+    {
+        Task<IReadOnlyList<Candidate>> GetByElectionIdAsync(Guid electionId);
+        Task<Candidate?> GetByIdAsync(Guid id);
+        Task AddAsync(Candidate candidate);
+        Task UpdateAsync(Candidate candidate);
+        Task DeleteAsync(Candidate candidate);
+    }
+}
