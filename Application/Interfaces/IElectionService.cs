f6248c4eab32aa2eee9e17295b3ebd24d59c8b28
RuddyG10 <rggt0001@ce.pucmm.edu.do>
2026-08-25 09:54:38 -0400

    feat: add election service interface

---
+++ Application/Interfaces/IElectionService.cs
@@ -0,0 +1,14 @@
+﻿using Application.DTOs.Elections;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Interfaces
+{
+    public interface IElectionService
+    {
+        Task<IReadOnlyList<ElectionResponse>> GetAllAsync();
+        Task<ElectionResponse> GetByIdAsync(Guid id);
+        Task<ElectionResponse> CreateAsync(CreateElectionRequest request);
+    }
+}
--- Application/obj/Debug/net10.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net10.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0243298f8e741ca1327f3a7e2d857da71429de2f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2f6ad5f19b134fdc92472407e4ab9e1b09b6f2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
