c4fb96aa461feaa2f1263e951fa7aa0b37f260f2
RuddyG10 <rggt0001@ce.pucmm.edu.do>
2026-08-24 20:37:16 -0400

    Created Voting DB Context for the database and added NuGets

--- Infrastructure/obj/Debug/net10.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net10.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+992d56fd310060087b3e91af25e5f56144fba632")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c5f31bbf27527401fb5ea94957359ff83d7cc26")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
